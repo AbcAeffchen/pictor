34493469f68de394404ac44226f701eb3ead459d
AbcAeffchen <alex@abcaeffchen.net>
2018-01-12 16:28:32 +0100

    Added first non customizable image generator.

--- pictor/core.py
+++ pictor/core.py
@@ -1,22 +1,116 @@
 import argparse
+import helpers
+import random
 
 
 class Pictor:
-    @staticmethod
-    def parse_arguments():
+
+    def parse_arguments(self):
         parser = argparse.ArgumentParser(description="Generate pixel art as vector image.")
 
-        parser.add_argument("-c", "--color", help="The color value as #rgb.")
-        parser.add_argument("-o", "--out", help="The path to the output file.")
-        parser.add_argument("-v", "--verbose", help="Print additional outputs", action="store_true")
+        parser.add_argument("-c", "--color", type=str,
+                            help="The color value as #rgb.")
+        parser.add_argument("-d", "--dim", nargs=2, type=int, default=[10, 15],
+                            help="dimension in pixels as width and height.")
+        parser.add_argument("-o", "--out", type=str, default="out.svg",
+                            help="The path to the output file.")
+        parser.add_argument("-v", "--verbose", action="store_true",
+                            help="Print additional outputs")
         parser.add_argument("--version", action="version", version="Pictor 0.1")
 
-        return parser.parse_args()
+        args = parser.parse_args()
+
+        if args.verbose:
+            print(args)
+
+        # validate inputs
+        if args.color is not None and not helpers.check_rgb_format(args.color):
+            print("Color needs to be in RGB format")
+            return False, None
+
+        return True, args
 
     def __init__(self):
-        self.args = self.parse_arguments()
-        if self.args.verbose:
-            print("Verbose is on")
+        # parse arguments
+        valid, self.args = self.parse_arguments()
+
+        if not valid:
+            quit(1)
+
+        # depending on the arguments create the right image
+        dims = self.get_image_dimensions()
+        image = self.create_random_image(dims, "#ABCABC")
+
+        self.export_svg(image, dims)
+
+    def get_image_dimensions(self):
+        """
+        Calculates the dimensions of the image in pixels depending on the arguments.
+        :return:
+        """
+
+        # todo pixel size needs to be adjusted if the subpixels and pixels off the grid are introduced.
+        return {"x": self.args.dim[0],
+                "y": self.args.dim[1],
+                "pixel_size": 1}
+
+    def create_random_image(self, dims, rgb_color):
+        """
+
+        :param dims: Image dimensions
+        :param rgb_color: A color in RGB format.
+        :return:
+        """
+
+        cmyk_color = helpers.rgb_to_cmyk(rgb_color)
+
+        image = {"background": "#FFFFF",  # todo make background optional
+                 "pixels": []}
+
+        contrast_ragne_factor = {"min": 0.5, "max": max(1, 0.9 / cmyk_color[3])}
+        # todo adjust number of steps, currently fixed to 10
+        contrast_step_size = (contrast_ragne_factor["max"] - contrast_ragne_factor["min"]) / 10
+
+        contrasts = [cmyk_color[3] * (contrast_ragne_factor["min"] + step * contrast_step_size) for step in
+                     range(0, 10)]
+
+        for x in range(0, dims["x"]):
+            for y in range(0, dims["y"]):
+                image["pixels"].append({"x": x, "y": y, "size": dims["pixel_size"],
+                                        "color": helpers.cmyk_to_rgb((cmyk_color[0], cmyk_color[1], cmyk_color[2],
+                                                                      random.choice(contrasts)
+                                                                      ))
+                                        })
+
+        return image
+
+    def export_svg(self, image, dims):
+        """
+
+        :param image:
+        :return:
+        """
+
+        f = open(self.args.out, 'w')
+
+
+
+        # open file
+        f.write("<svg viewbox=\"0 0 {1} {0}\" xmlns=\"http://www.w3.org/2000/svg\">".format(dims["x"] * 32, dims["y"] * 32))
+
+        # write pixels
+        for pixel in image["pixels"]:
+            f.write("<rect height=\"{0}\" width=\"{0}\" x=\"{2}\" y=\"{1}\" style=\"fill: rgb({3},{4},{5}); stroke-width: 0\"/>"
+                    .format(pixel["size"] * 32, pixel["x"] * 32, pixel["y"] * 32, pixel["color"][0], pixel["color"][1], pixel["color"][2]
+                            )
+                    )
+
+        # close file
+        f.write("</svg>")
+        f.close()
+
+
+        # todo image is a list of pixels. write them into a file.
 
 
 Pictor()
--- pictor/helpers.py
+++ pictor/helpers.py
@@ -2,7 +2,7 @@
 
 def check_rgb_format(rgb_string):
     """
-    TODO
+    Checks if rgb_string is actually a rgb string formatted as #[0-9a-fA-F]{6}
     :param rgb_string:
     :return: bool
     """
@@ -19,7 +19,7 @@ def check_rgb_format(rgb_string):
 
 def rgb_to_cmyk(rgb_string):
     """
-    TODO
+    Converts a rgb string to a tuple representing a CMYK color.
     :param rgb_string:
     :return: (c, y, m, k) A tuple of floats in [0,1]
     """
@@ -28,9 +28,27 @@ def rgb_to_cmyk(rgb_string):
     g = int(rgb_string[3:5], 16) / 255
     b = int(rgb_string[5:7], 16) / 255
 
-    k = max(r, g, b)
+    # print(r,g,b)
+
+    k = 1 - max(r, g, b)
     c = (1 - r - k) / (1 - k)
     m = (1 - g - k) / (1 - k)
     y = (1 - b - k) / (1 - k)
 
-    return c, y, m, k
+    # print(c,m,y,k)
+
+    return c, m, y, k
+
+
+def cmyk_to_rgb(cmyk):
+    """
+    Converts a CMYK color to a RGB color as tuple of integers in [0, 255]
+    :param cmyk: CMYK color (as tuple)
+    :return: (r,g,b) RGB color  as tuple of integers in [0, 255]
+    """
+
+    r = int(255 * (1-cmyk[0]) * (1-cmyk[3]))
+    g = int(255 * (1-cmyk[1]) * (1-cmyk[3]))
+    b = int(255 * (1-cmyk[2]) * (1-cmyk[3]))
+
+    return r, g, b
